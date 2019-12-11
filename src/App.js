d155d923a82d8921efcd02fd708b60e3ac1a4852
Mykenzie Rogers <mykenzierogers@gmail.com>
2019-12-11 10:13:40 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./reset.css";
+
+//TOTAL POINTS - 4;
 
 function App() {
   return (
